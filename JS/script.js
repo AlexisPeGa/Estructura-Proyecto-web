a565581318799901167f32f4962124eeb75fc4f1
AlexisPeGa <alexispe8@gmail.com>
2021-09-28 16:26:09 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+
+window.onload=function (){
+alert("load")
+}
+
+nombre.addEventListener("mouseover", function () {
+
+    nombre.style.color = "green";
+    nombre.innerText = (nombre.innerText).toUpperCase();
+
+}, false)
+nombre.addEventListener("mouseout", function () {
+    nombre.style.color = "black";
+    nombre.innerText = (nombre.innerText).toLowerCase();
+
+}, false)
