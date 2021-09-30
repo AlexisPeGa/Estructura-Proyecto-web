ce3eed786812db49d9f825151ffb3ca60cdf2b67
AlexisPeGa <alexispe8@gmail.com>
2021-09-30 08:50:39 +0200

    master

--- UT2/conversionTiposNumeros/JS/funciones.js
+++ UT2/conversionTiposNumeros/JS/funciones.js
@@ -2,14 +2,16 @@ window.onload=function(){
     var opcion = document.getElementById("modo");
 }
 
-opcion.addEventListener("change", function(){
+opcion.addEventListener("change", 
+function(){
     var elemento=this.options[this.selectedIndex].value;
-    var numero.document.getElementById("numero").value;
-    var resultado.document.getElementById("resultado").value;
+    var numero=document.getElementById("numero").value;
+    var resultado=document.getElementById("resultado").value;
     switch(elemento){
         case"1":
             //codigo que pasa de decimal a binario
-            
+            nn=parseInt(numero)
+            alert(nn);
         break;
         case"2":
 
