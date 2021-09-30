e70634579ca1b7f51d842b3f6ce1836014722e9d
AlexisPeGa <alexispe8@gmail.com>
2021-09-30 08:57:58 +0200

    a

--- UT2/conversionTiposNumeros/JS/funciones.js
+++ UT2/conversionTiposNumeros/JS/funciones.js
@@ -6,12 +6,14 @@ opcion.addEventListener("change",
 function(){
     var elemento=this.options[this.selectedIndex].value;
     var numero=document.getElementById("numero").value;
+    var nn=parseInt(numero)
     var resultado=document.getElementById("resultado").value;
+    
     switch(elemento){
         case"1":
             //codigo que pasa de decimal a binario
-            nn=parseInt(numero)
-            alert(nn);
+            
+            resultado.value=nn.toString(2);
         break;
         case"2":
 
