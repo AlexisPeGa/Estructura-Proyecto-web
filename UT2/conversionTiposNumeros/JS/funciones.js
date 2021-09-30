55d5dc80ab45f3bf4cfbcd7b4f811b6851813f09
AlexisPeGa <alexispe8@gmail.com>
2021-09-30 09:11:23 +0200

    a

--- UT2/conversionTiposNumeros/JS/funciones.js
+++ UT2/conversionTiposNumeros/JS/funciones.js
@@ -1,28 +1,38 @@
-window.onload=function(){
-    var opcion = document.getElementById("modo");
-}
+var opcion =document.getElementById("modo");
 
-opcion.addEventListener("change", 
-function(){
+opcion.addEventListener("change",calcular,false);
+
+function calcular(){
     var elemento=this.options[this.selectedIndex].value;
-    var numero=document.getElementById("numero").value;
-    var nn=parseInt(numero)
-    var resultado=document.getElementById("resultado").value;
-    
-    switch(elemento){
-        case"1":
-            //codigo que pasa de decimal a binario
-            
-            resultado.value=nn.toString(2);
-        break;
-        case"2":
-
-        break;
-        case"3":
-
-        break;
-        case"4":
-
-        break;
+    var numero= document.getElementById("numero").value;
+    var nn=parseInt(numero) 
+    var resultado= document.getElementById("resultado") ;
+    switch (elemento)
+    {
+        case "1":
+            // Código que pasa de decimal a binario       
+           resultado.value=nn.toString(2);           
+         break;
+         case "2":
+            // Código que pasa de decimal a octal       
+           resultado.value=nn.toString(8);
+         break;
+         case "3":
+            // Código que pasa de decimal a hexadecimal       
+           resultado.value=nn.toString(16);
+         break;
+         case "4":
+            // Código que pasa de decimal a hexadecimal       
+           resultado.value=nn.toString(16);
+         break;
+         case "5":
+            // Código que pasa de decimal a hexadecimal       
+           resultado.value=nn.toString(16);
+         break;
+         case "6":
+            // Código que pasa de decimal a hexadecimal       
+           resultado.value=nn.toString(16);
+         break;
     }
-},false);
+
+}
