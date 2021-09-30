f4a81f1a05db849898202dfb4df30b8d86c27578
AlexisPeGa <alexispe8@gmail.com>
2021-09-30 08:47:51 +0200

    a

---
+++ UT2/conversionTiposNumeros/JS/funciones.js
@@ -0,0 +1,24 @@
+window.onload=function(){
+    var opcion = document.getElementById("modo");
+}
+
+opcion.addEventListener("change", function(){
+    var elemento=this.options[this.selectedIndex].value;
+    var numero.document.getElementById("numero").value;
+    var resultado.document.getElementById("resultado").value;
+    switch(elemento){
+        case"1":
+            //codigo que pasa de decimal a binario
+            
+        break;
+        case"2":
+
+        break;
+        case"3":
+
+        break;
+        case"4":
+
+        break;
+    }
+},false);
