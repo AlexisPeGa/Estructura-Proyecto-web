9c6a77644ef327553cae8677b3c01fe67011ac15
AlexisPeGa <alexispe8@gmail.com>
2021-10-01 18:15:23 +0200

    a

--- UT2/conversionEspañolChino/JS/funciones.js
+++ UT2/conversionEspañolChino/JS/funciones.js
@@ -1,1 +1,13 @@
-var chino = unescape("%u" + codigoUnicode + "e8"); 
+var letra=document.getElementById("texto");
+letra.onkeyup = function(){convertir()};
+
+function convertir(){
+
+var cadena = document.getElementById("texto").value;   
+var posicion = cadena.length - 1;             
+  var codigoUnicode = cadena.charCodeAt(posicion);
+  var chino = unescape("%u" + codigoUnicode + "e8");
+  cadena = cadena.substring(0, cadena.length - 1);
+  cadena = cadena + chino;
+  document.getElementById("texto").value = cadena;
+}
