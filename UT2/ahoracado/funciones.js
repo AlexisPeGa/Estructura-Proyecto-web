f034c130a4b44beb2646efd92aacb25fe279801c
AlexisPeGa <alexispe8@gmail.com>
2021-10-06 08:28:58 +0200

    a

---
+++ UT2/ahoracado/funciones.js
@@ -0,0 +1,36 @@
+var palabra=document.getElementById("palabra");
+var letra=document.getElementById("letra");
+var aciertos=document.getElementById("aciertos");
+var fallos=document.getElementById("fallos");
+palabra.addEventListener("blur",crearGuiones,false);
+letra.addEventListener("keyup",buscarLetra,false);
+
+var guiones;
+
+function crearGuiones(){
+    guiones=palabra.value;
+
+    //reemplaza cualquier carácter entre a-z , 
+    //g hace que se repita con todos los caracateres del string
+    //i que no diferencie entre mayúsculas y minúsculas
+
+    guiones=guiones.replace(/[a-z]/gi,"-");
+    aciertos.value=guiones;
+}
+function buscarLetra(){
+    if(letra.value==""){
+        return;
+    }
+    var letraBuscar=letra.value.toUpperCase();
+    var posicion = palabra.indexOf(letraBuscar);
+    var esAcierto=false;
+    while(posicion>-1){
+        guiones=guiones.substring(0,posicion)+letraBuscar+guiones.substr(posicion+1,guiones.length);
+        aciertos.value=guiones;
+        posicion=palabra.indexOf(letraBuscar,posicion+1);
+    }
+    if(!esAcierto){
+        fallos.innerHTML=fallos.innerHTML+"<b>"+letraBuscar+"</b>";
+    }
+    letra.value="";
+}
