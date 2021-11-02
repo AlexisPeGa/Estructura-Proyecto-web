b524cc136cbfedd1e3be3d7e8ebce912b8976d54
AlexisPeGa <alexispe8@gmail.com>
2021-11-02 18:04:03 +0100

    a

--- UT2/EPrimera-Enunciado/js/datos.js
+++
@@ -1,5 +0,0 @@
-//Array para los registros del fichero xml
-var datos = new Array();
-// Array de marcadores de Google Map para pòder borrarles
-var marcadores =  new Array();
- 
--- UT2/EPrimera-Enunciado/js/funcionesMapa.js
+++ UT2/EPrimera-Enunciado/js/funcionesMapa.js
@@ -1,22 +1,17 @@
-window.onload = function () {
-   
-    files.addEventListener("change", leeFicheroLocal, true);
-    
-}
-
-
-
 //Array para los registros del fichero xml
 var datos = new Array();
 // Array de marcadores de Google Map para pòder borrarles
-var marcadores =  new Array();
-
-var contador = 0;
-var pos = 0;
-///////// CERAR OBJETO//////////////
+var marcadores = new Array();
+var seleccion = document.getElementById("FamiliaSeleccionada");
+var datosCopia = new Array();
+///////// CREAR OBJETO//////////////
 var mobiliario = new Object();
+var aseleccion = false;
 
-function datosmobiliario(ID, DESCRIPCION, LATITUD, LONGITUD,IMAGEN,FECHAMANTENIMIENTO,FAMILIA) {
+files.addEventListener("change", leeFicheroLocal, true);
+seleccion.addEventListener("change", opcion, true);
+
+function datosmobiliario(ID, DESCRIPCION, LATITUD, LONGITUD, IMAGEN, FECHAMANTENIMIENTO, FAMILIA) {
     this.ID = ID;
     this.DESCRIPCION = DESCRIPCION;
     this.LATITUD = LATITUD;
@@ -28,14 +23,38 @@ function datosmobiliario(ID, DESCRIPCION, LATITUD, LONGITUD,IMAGEN,FECHAMANTENIM
     this.guarda = guardadatos;
 
 }
-///////// CERAR OBJETO//////////////
+///////// CREAR OBJETO//////////////
+
+function opcion() {
+    datosCopia = new Array();
+
+    for (i = 0; i < datos.length; i++) {
+
+        if (datos[i].FAMILIA == seleccion.value) {
+
+            datosCopia.push(datos[i]);
+        }
+        console.log(datosCopia[i]);
+
+    }
+    switch (seleccion.value) {
+        case "defecto":
+            aseleccion = false;
+            imprimirentabla(datos);
+            break;
+        default:
+            aseleccion = true;
+            imprimirentabla(datosCopia);
+            break;
+    }
+    inicio();
+}
+
+
 
 ////////////REGOGER DATOS/////////
 function guardadatos() {
-    /*alert(this.ID);
-    datos[contador] = this;
-    contador = contador + 1;
-    pos = contador;*/
+
     datos.push(this);
 }
 function leeFicheroLocal(evt) {
@@ -47,7 +66,7 @@ function leeFicheroLocal(evt) {
 
     var reader = new FileReader();
     reader.onload = function (event) {
-        
+
         datos = new Array();
         var contenido = event.target.result;
         //  alert("contenido" + contenido)
@@ -72,25 +91,20 @@ function leeFicheroLocal(evt) {
         aFamilia = myXml.getElementsByTagName("familia");
         //alert(aindice.length);
         for (var i = 0; i < aId.length; i++) {
-            
-            p = new datosmobiliario(aId.item(i).firstChild.nodeValue, aDescripcion.item(i).firstChild.nodeValue,  aLatitud.item(i).firstChild.nodeValue, aLongitud.item(i).firstChild.nodeValue, aImagen.item(i).firstChild.nodeValue, aFechaMantenimiento.item(i).firstChild.nodeValue, aFamilia.item(i).firstChild.nodeValue);
-            
+
+            p = new datosmobiliario(aId.item(i).firstChild.nodeValue, aDescripcion.item(i).firstChild.nodeValue, aLatitud.item(i).firstChild.nodeValue, aLongitud.item(i).firstChild.nodeValue, aImagen.item(i).firstChild.nodeValue, aFechaMantenimiento.item(i).firstChild.nodeValue, aFamilia.item(i).firstChild.nodeValue);
+
             datos[i] = p;
 
         }
-        c = i; contador = c;
-        visualiza(0);
 
-        // En esta fucntión generaTablaLocales es donde yo genero la tabla y los marcadores del mapa
-
-        //generaTablaLocales();
 
         // Permite que se ejecute de nuevo el evento change, ya que siempre trabajamos con el mismo fichero xml
         evt.target.value = ''
-        imprimirentabla();
-        
-        inicio();
-        visualizaMarcador();
+        //llmada de funciones
+        opcion();
+
+
     }
 
     reader.onerror = function (event) {
@@ -102,28 +116,15 @@ function leeFicheroLocal(evt) {
 
 }
 
-function visualiza(pos) {
-    mobiliario = new datosmobiliario();
-    mobiliario = datos[pos];
-    if (mobiliario == undefined) { return; }
-
-    document.getElementById("cId").value = mobiliario.ID;
-    document.getElementById("cDescripcion").value = mobiliario.DESCRIPCION;
-    document.getElementById("cLatitud").value = mobiliario.LATITUD;
-    document.getElementById("cLongitud").value = mobiliario.LONGITUD;
-    document.getElementById("cImagen").value = mobiliario.IMAGEN;
-    document.getElementById("cFechaMantenimiento").value = mobiliario.FECHAMANTENIMIENTO;
-    document.getElementById("cFamilia").value = mobiliario.FAMILIA;
-
-}
 ////////////REGOGER DATOS/////////
 
 ///////////VISUALIZAR EN TABLA/////////
-function imprimirentabla() {
-   
-    for (c = 0; c < contador; c++) {
+
+function imprimirentabla(Arraydatos) {
+    document.getElementById("cuerpo").innerHTML = "";
+    for (c = 0; c < Arraydatos.length; c++) {
         var da = new datosmobiliario();
-        da = datos[c];
+        da = Arraydatos[c];
         var tabla = document.getElementById("tabla");
         var cuerpo = document.getElementById("cuerpo");
         linea = document.createElement("tr");
@@ -163,48 +164,55 @@ function imprimirentabla() {
 var map;
 var latitud = 41.67097948393865;
 var longitud = -3.6769259916763985;
-function inicio()
-{
- 
-map = new google.maps.Map(
-    document.getElementById('map_canvas'), {
-    // En el mapa se visualiza el mapa correspondiente a esta latitud, longitud
-        center: new google.maps.LatLng(latitud,longitud),//latitud,longitud),//
-       // center: new google.maps.LatLng(41.6685198,-3.6886618),//latitud,longitud),//
-    zoom: 18, // zoom del mapa
-    draggableCursor: 'auto', // forma del cursor
-    draggingCursor: 'crosshair',
-    mapTypeId: google.maps.MapTypeId.SATELLITE // tipo de mama
-});
 
-
-
-google.maps.event.addListener(map, 'click', function (event) {
-
-    datolatitud_longitud = event.latLng.toString();
+function inicio() {
+    marcadores = new Array();
+    map = new google.maps.Map(
+        document.getElementById('map_canvas'), {
+        // En el mapa se visualiza el mapa correspondiente a esta latitud, longitud
+        center: new google.maps.LatLng(latitud, longitud),//latitud,longitud),//
+        // center: new google.maps.LatLng(41.6685198,-3.6886618),//latitud,longitud),//
+        zoom: 18, // zoom del mapa
+        draggableCursor: 'auto', // forma del cursor
+        draggingCursor: 'crosshair',
+        mapTypeId: google.maps.MapTypeId.SATELLITE // tipo de mama
+    });
     var fileName = "./imagenes/Logo.png";
 
-    var icono = {
-        url: "./imagenes/curso.png", // url
-        scaledSize: new google.maps.Size(25, 25), // scaled size
-        origin: new google.maps.Point(0, 0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
-    };
 
-    leeDireccion(event.latLng);
-});
+    if (aseleccion) {
+        crearmarcador(datosCopia);
+    } else {
+        crearmarcador(datos);
+    }
+    google.maps.event.addListener(map, 'click', function (event) {
+
+
+        datolatitud_longitud = event.latLng.toString();
+        
+        
+        
+        //leeDireccion(event.latLng);
+    });
+    
+
 }
+/*
 // Obtiene la longitud y la latitud correspondiente al clic
 // y copia los datos en cajas de texto. Tambien obtiene la
 // dirección del lugar donde hacemos clic
 function leeDireccion(latlng) {
     geocoder = new google.maps.Geocoder();
     if (latlng != null) {
-    
+        //    address = latlng;
+        //    geocoder.getLocations(latlng, MuestraDireccion);
+
         geocoder.geocode({ 'latLng': latlng }, function (results, status) {
             if (status == google.maps.GeocoderStatus.OK) {
                 if (results[1]) {
-                   
+                    //https://developers.google.com/maps/documentation/javascript/geocoding?hl=es
+                    //  alert(results[1].formatted_address);
+                    //  alert(results[0].formatted_address);
                     MuestraDireccion(latlng, results[0].formatted_address)
                 } else {
                     alert('No results found');
@@ -216,56 +224,56 @@ function leeDireccion(latlng) {
 
     }
 }
+*/
+/*function MuestraDireccion(latlng, direccion) {
 
-function MuestraDireccion(id,descripcion,latlng, imagen,fechaMantenimiento,familia) {
-    document.getElementById('cId').value = id;
-    document.getElementById('cDescripcion').value = descripcion;
-    document.getElementById('cLatitud').value = latlng.lat();
-    document.getElementById('cLongitud').value = latlng.lng();
-    document.getElementById('cImagen').value = imagen;
-    document.getElementById('cFechaMantenimiento').value = fechaMantenimiento;
-    document.getElementById('cFamilia').value = familia;
+    direccionLocal.value = direccion;
+
+    latitud_longitud.value = latlng.lat() + "," + latlng.lng();
+}*/
+
+// Para poder borrar los marcadores es necesario almacenarlos en un array
+function borrarMarcadores() {
+    // Elimina los marcadores de una consulta anterior
+    for (var i = 0; i < marcadores.length; i++) {
+        marcadores[i].setMap(null);
+    }
+    marcadores = new Array();
 }
 
+function crearmarcador(Arraydatos) {
+    for (i = 0; i < Arraydatos.length; i++) {
+        var icono = {
+            url: Arraydatos[i].IMAGEN, // url
+            scaledSize: new google.maps.Size(25, 25), // scaled size
+            origin: new google.maps.Point(0, 0), // origin
+            anchor: new google.maps.Point(0, 0) // anchor
+        };
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng( Arraydatos[i].LATITUD,Arraydatos[i].LONGITUD),
+            icon: icono,
+            id: Arraydatos[i].ID,
+            descripcion: Arraydatos[i].DESCRIPCION,
+            latitud: Arraydatos[i].LATITUD,
+            longitud: Arraydatos[i].LONGITUD,
+            imagen: Arraydatos[i].IMAGEN,
+            fechaMantenimiento: Arraydatos[i].FECHAMANTENIMIENTO,
+            familia: Arraydatos[i].FAMILIA,
+            map: map,
+            nombre: 'Pepino'
+        });
+        // Almacena el marcador para después poder borrarles
+        marcadores.push(marker);
+        google.maps.event.addListener(marker, 'click', function () {
+            document.getElementById("cId").value=this.id;
+            document.getElementById("cDescripcion").value=this.descripcion;
+            document.getElementById("cLatitud").value=this.latitud;
+            document.getElementById("cLongitud").value=this.longitud;
+            document.getElementById("cImagen").value=this.imagen;
+            document.getElementById("cFechaMantenimiento").value=this.fechaMantenimiento;
+            document.getElementById("cFamilia").value=this.familia;
+        });
+    }
 
 
-
-
-/////////////MAPA//////////////
-///////////////MARCADORES///////
-function visualizaMarcador(datos){
-
-    //CREAMOS UN ICONO CON LA RUTA DE LA IMAGEN
-    var icono = {
-        //Es la imagen que cuando hacemos clic hacemos un marcador con la imagen
-        url: "./imagenes/curso.png", // url
-        //url: mobiliario.imagen,
-        scaledSize: new google.maps.Size(25, 25), // scaled size TAMAÑO DE LA IMAGEN
-        origin: new google.maps.Point(0,0), // origin
-        anchor: new google.maps.Point(0, 0) // anchor
-    };
-
-    //CREAMOS LA LATITUD Y LA LONGITUD
-    var latlog = new google.maps.LatLng(datos.latitud, datos.longitud);
-
-    //CREAMOS UN MARCADOR
-    marker = new google.maps.Marker({
-        //POSICION CON LATITUD Y LONGITUD
-        position: latlog,
-        icon: icono,
-        map: map,
-        nombre: datos
-    });
-    marker.addListener("click", function(){
-        if(datos==undefined){return;}
-            document.getElementById("cId").value = datos.id;
-            document.getElementById("cDescripcion").value = datos.Descripcion;
-            document.getElementById("cLatitud").value = datos.latitud;
-            document.getElementById("cLongitud").value = datos.longitud;
-            document.getElementById("cImagen").value = datos.imagen;
-            document.getElementById("cFechaMantenimiento").value = datos.fechaMantenimiento;
-            document.getElementById("cFamilia").value = datos.familia;
-    });
-    
 }
-///////////////MARCADORES///////
